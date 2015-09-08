c9be429d232966fb6dfb4e62f729e3d6e8ac31e4
saanindia <saan.india@gmail.com>
2015-09-08 05:28:37 +0000

    Initial project

---
+++ build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+bundle install
+
+bundle exec veewee vbox build 'centos63' --force --auto --nogui
+bundle exec veewee vbox validate 'centos63'
+
+bundle exec vagrant basebox export 'centos63' --force
